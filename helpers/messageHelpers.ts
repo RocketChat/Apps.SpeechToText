d68709f162c66ab3cd625acf3e24aa057a5f6473
kartik <gupta.kartik18kg@gmail.com>
2021-06-25 02:10:37 +0530

    remove redundant comments

--- helpers/messageHelpers.ts
+++ helpers/messageHelpers.ts
@@ -32,28 +32,8 @@ export const updateSttMessage = async (data, sender: IUser, modify: IModify) => 
         const { messageId } = data
         const messageUpdater = modify.getUpdater()
         let builder = await messageUpdater.message(messageId, sender!)
-        // modifySttAttachment(data,sender,builder)
-        // let attachments = builder.getAttachments()
-        // attachments = removeSttAttachment(attachments)
-
-        // builder.setAttachments([...attachments, {
-        //     title: { value: "SpeechToText" },
-        //     text,
-        //     color,
-        //     actionButtonsAlignment: MessageActionButtonsAlignment.HORIZONTAL,
-        //     actions: button === true ? [
-        //         {
-        //             text: buttonText,
-        //             type: MessageActionType.BUTTON,
-        //             msg_in_chat_window: true,
-        //             msg: buttonMessage,
-        //         },
-        //     ] : [],
-        // }]).setEditor(sender)
-
-
-        // sendMessage(modify, room, { text: "File Queued for transcription", userId: context.getSender().id })
         await messageUpdater.finish(modifySttAttachment(data, sender, builder))
+
     } catch (error) {
         console.log(error)
     }
