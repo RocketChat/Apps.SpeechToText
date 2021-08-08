517f534bfc17406ec71b036ce0dba0b6c5507d88
kartik gupta <gupta.kartik18kg@gmail.com>
2021-08-09 00:39:37 +0530

    Refactor AssemblyAI provider

--- lib/providers/Assembly.ts
+++ lib/providers/Assembly.ts
@@ -1,6 +1,5 @@
 import { IHttp, IRead, IModify } from "@rocket.chat/apps-engine/definition/accessors";
-import { IApi, IApiRequest, IApiResponse } from "@rocket.chat/apps-engine/definition/api";
-import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { IApiRequest, IApiResponse } from "@rocket.chat/apps-engine/definition/api";
 import { generateJWT, getPayload } from "../../helpers/jwtHelpers";
 import { updateSttMessage } from "../../helpers/messageHelpers";
 import { SpeechToTextApp } from "../../SpeechToTextApp";
@@ -8,20 +7,17 @@ import { SttInterface } from "../interface/SttInterface";
 
 export class Assembly implements SttInterface {
 
-    public sender: String
 
     constructor(private readonly app: SpeechToTextApp) {
-        this.sender = this.app.getID()
     }
-
-    public host = "https://2b9ace0860bf.ngrok.io"
 
     async handleWebhook(success, request: IApiRequest, http: IHttp, read: IRead, modify: IModify): Promise<IApiResponse> {
         await this.getTranscript(request.content, http, read, modify)
         return success();
     }
 
-    async queueAudio(data: any, http: IHttp, read: IRead, modify: IModify): Promise<Boolean> {
+    async queueAudio(data: any, http: IHttp, read: IRead): Promise<{ status: Boolean; message: String }> {
+        // console.log("This is the PAPAPPAPAPAPA", this.sender)
         // destructure data
         const { rid, fileId, messageId, userId, audioUrl } = data;
         const api_key: string = await read
@@ -44,8 +40,8 @@ export class Assembly implements SttInterface {
             audioUrl
         }, jwt_secret)
         // Appending the JWT token to audioURL and getting the final recording URL which is to be sent to the provider
-        let recordingUrl = `${this.host}${audioUrl}?token=${jwtToken}`;
-        let webhook_url = `${this.host}/api/apps/public/0cb2ef83-b652-4862-9331-275ccbf2bfa7/stt-webhook`;
+        let recordingUrl = `${this.app.host}${audioUrl}?token=${jwtToken}`;
+        let webhook_url = `${this.app.host}${this.app.webhook_url}`;
         let reqUrl = "https://api.assemblyai.com/v2/transcript";
 
         let response = await http.post(reqUrl, {
@@ -59,39 +55,44 @@ export class Assembly implements SttInterface {
             },
         });
         if (response && response.data.status === "queued") {
-            return true
+            return { status: true, message: "Queued for transcriptionn" }
         }
-        return false
-
+        return { status: false, message: response.data.error }
     }
 
 
     async getTranscript(data: any, http: IHttp, read: IRead, modify: IModify): Promise<void> {
-        const { transcript_id, status } = data
+        const { transcript_id } = data
         const reqUrl = `https://api.assemblyai.com/v2/transcript/${transcript_id}`
         const api_key: string = await read
             .getEnvironmentReader()
             .getSettings()
             .getValueById("api-key");
+        // console.log(data)
         var response = await http.get(reqUrl, {
             headers: {
                 ["authorization"]: `${api_key}`,
                 ["content-type"]: "application/json",
             },
         });
-        const responseData = response.data
-        const { audio_url, text } = responseData
+        // const responseData = response.data
+        // const {status} = response.data
+        // console.log(response.data)
+        const { audio_url, text, status, error } = response.data
+
         const token = audio_url.split('token=')[1]
         const payload = getPayload(token.split("&")[0])
 
-        const sender = await read.getUserReader().getAppUser(this.app.getID())
+        const botUser = await read.getUserReader().getAppUser(this.app.getID())
         const { messageId, rid, fileId } = payload.context
-        if (status === "completed") {
-            updateSttMessage({ messageId, text, color: "#800080" }, sender!, modify)
+        if (status === "error") {
+            if (error.includes("Download error")) {
+                updateSttMessage({ text: "Transcription failed !! JWT in app setting doesn't match the FileUpload JWT ", color: "#dc143c", messageId, button: true, buttonText: "ReQueue", buttonMessage: `/stt-queue ${rid} ${fileId} ${messageId} ${payload.context.audioUrl}` }, botUser!, modify)
+            } else {
+                updateSttMessage({ text: "Transcription failed !! ", color: "#dc143c", messageId, button: true, buttonText: "ReQueue", buttonMessage: `/stt-queue ${rid} ${fileId} ${messageId} ${payload.context.audioUrl}` }, botUser!, modify)
+            }
         } else {
-            // bug url/url/url need to slice the url to just get '/fileupload${fileID}-${filename}
-            // bug fix
-            updateSttMessage({ text: "Failed, try again !!", color: "#dc143c", messageId, button: true, buttonText: "ReQueue", buttonMessage: `/stt-queue ${rid} ${fileId} ${messageId} ${payload.context.audioUrl}` }, sender!, modify)
+            updateSttMessage({ messageId, text, color: "#800080" }, botUser!, modify)
         }
 
     }
