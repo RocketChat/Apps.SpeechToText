dccedc4a2c9ecb259ba5e0d0cda3b221f72e866e
kartik gupta <gupta.kartik18kg@gmail.com>
2021-08-09 00:37:30 +0530

    added tunnel setting

--- SpeechToTextApp.ts
+++ SpeechToTextApp.ts
@@ -13,109 +13,116 @@ import { ApiSecurity, ApiVisibility } from '@rocket.chat/apps-engine/definition/
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IMessage, IPreMessageSentExtend, MessageActionButtonsAlignment, MessageActionType } from '@rocket.chat/apps-engine/definition/messages';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-import { ISetting } from '@rocket.chat/apps-engine/definition/settings';
+import { ISetting, SettingType } from '@rocket.chat/apps-engine/definition/settings';
 import { QueueAudioCommand } from './commands/SttCommand';
 import { AppSetting, settings } from './config/Setting';
 import { webhookEndpoint } from './endpoints/webhookEndpoint';
 import { getAudioAttachment, isAudio } from './helpers/attachmentHelpers';
 import { Assembly } from './lib/providers/Assembly';
 import { Microsoft } from './lib/providers/Microsoft';
-import { Rev } from './lib/providers/Rev';
 
 export class SpeechToTextApp extends App implements IPreMessageSentExtend {
-
 
     // username alias
     public botName: string = "SpeechToText-BOT";
     //Avatar alias
     public botAvatar: string = ":microphone2:";
 
-    public webhook_url: string
-
     public host
 
     public provider
+
+    public webhook_url
 
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
 
-
-    /**
-     * Updates room ids for members and messages when settings are updated
-     *
-     * @param setting
-     * @param configModify
-     * @param read
-     * @param http
-     */
-    public async onSettingUpdated(
-        setting: ISetting,
-        configModify: IConfigurationModify,
-        read: IRead,
-        http: IHttp
-    ): Promise<void> {
-        if (setting.id === "api-provider") {
-            switch (setting.value) {
-                case AppSetting.ASSEMBLY:
-                    this.provider = new Assembly(this)
-                    break;
-                case AppSetting.MICROSOFT:
-                    this.provider = new Microsoft(this)
-                    this.provider.registerWebhook(http, read)
-                    break;
-                case AppSetting.REV:
-                    this.provider = new Rev(this)
-                    break;
-            }
-        }
-    }
-
-    /**
-     * @param environmentRead
-     * @param configModify
-     */
     public async onEnable(
         environmentRead: IEnvironmentRead,
         configModify: IConfigurationModify
     ): Promise<boolean> {
         const [webhook] = this.getAccessors().providedApiEndpoints.filter((endpoint) => endpoint.path === 'stt-webhook')
+
         this.webhook_url = webhook.computedPath
 
-        const setting = await environmentRead.getSettings().getById("api-provider")
-        switch (setting.value) {
+        const siteUrl = await environmentRead.getServerSettings().getValueById('Site_Url');
+        if (siteUrl.includes('localhost')) {
+            this.host = await environmentRead.getSettings().getValueById(AppSetting.TUNNEL)
+        }
+        const provider = await environmentRead.getSettings().getValueById("api-provider")
+        switch (provider) {
             case AppSetting.ASSEMBLY:
                 this.provider = new Assembly(this)
                 break;
             case AppSetting.MICROSOFT:
                 this.provider = new Microsoft(this)
                 break;
-            case AppSetting.REV:
-                this.provider = new Rev(this)
-                break;
         }
         return true;
     }
 
+
+    public async onSettingUpdated(
+        setting: ISetting,
+        configModify: IConfigurationModify,
+        read: IRead,
+        http: IHttp
+    ): Promise<void> {
+        if (setting.id === AppSetting.TUNNEL) {
+            this.host = setting.value
+        }
+        const provider = await read.getEnvironmentReader().getSettings().getValueById("api-provider")
+        switch (provider) {
+            case AppSetting.ASSEMBLY:
+                this.provider = new Assembly(this)
+                break;
+            case AppSetting.MICROSOFT:
+                this.provider = new Microsoft(this)
+                this.provider.registerWebhook(http, read)
+                break;
+        }
+    }
 
 
     protected async extendConfiguration(
         configuration: IConfigurationExtend,
         environmentRead: IEnvironmentRead
     ): Promise<void> {
+        // set site URL
+        const siteUrl = await environmentRead.getServerSettings().getValueById('Site_Url');
         // user settings
         await Promise.all(settings.map((setting) => configuration.settings.provideSetting(setting)));
+
+        if (siteUrl.includes('localhost')) {
+            await configuration.settings.provideSetting({
+                id: AppSetting.TUNNEL,
+                i18nLabel: "Tunnel URL",
+                i18nDescription: "Create a tunnel to your localhost and paste the url here for the app to work",
+                required: true,
+                type: SettingType.STRING,
+                public: true,
+                packageValue: "",
+            })
+        } else {
+            this.host = siteUrl.slice(0, -1)
+        }
+
         // API Webhook
-        configuration.api.provideApi({
+        await configuration.api.provideApi({
             visibility: ApiVisibility.PUBLIC,
             security: ApiSecurity.UNSECURE,
             endpoints: [new webhookEndpoint(this)],
         });
+
         // Slash command
         await configuration.slashCommands.provideSlashCommand(
             new QueueAudioCommand(this)
         );
+
     }
+
+
 
     public async checkPreMessageSentExtend(
         message: IMessage,
@@ -123,57 +130,34 @@ export class SpeechToTextApp extends App implements IPreMessageSentExtend {
         http: IHttp
     ): Promise<boolean> {
 
-        // return isAudio(message)
-        return true
+        return isAudio(message)
 
     }
 
     public async executePreMessageSentExtend(message: IMessage, extend: IMessageExtender, read: IRead, http: IHttp, persistence: IPersistence): Promise<IMessage> {
 
-
-        // check for settings
-        const api_key: string = await read
-            .getEnvironmentReader()
-            .getSettings()
-            .getValueById("api-key");
-        const api_provider: string = await read
-            .getEnvironmentReader()
-            .getSettings()
-            .getValueById("api-provider");
-        const jwt_secret: string = await read
-            .getEnvironmentReader()
-            .getSettings()
-            .getValueById("jwt-secret");
-
-        if (api_key.length > 0 && api_provider.length > 0 && jwt_secret.length > 0) {
-            // collect fields required
-
-            const audioAttachment = getAudioAttachment(message)
-            const rid = message.room.id
-            const fileId = message.file?._id
-            const messageId = message.id
-            const audioUrl = audioAttachment.audioUrl
-            // adding a slashcommand button with required fields
-            extend.addAttachment({
-                color: "#2576F5",
-                actionButtonsAlignment: MessageActionButtonsAlignment.HORIZONTAL,
-                title: { value: "SpeechToText" },
-                text: "Queue audio file for transcription",
-                actions: [
-                    {
-                        text: 'Transcribe',
-                        type: MessageActionType.BUTTON,
-                        msg_in_chat_window: true,
-                        msg: `/stt-queue ${rid} ${fileId} ${messageId} ${audioUrl}`,
-                    },
-                ],
-            })
-        } else {
-            // console.log("Settings not provided")
-            // sendMessage(modify, message.room.id, { message: "HIHI" })
-        }
-
+        const audioAttachment = getAudioAttachment(message)
+        const rid = message.room.id
+        const fileId = message.file?._id
+        const messageId = message.id
+        const audioUrl = audioAttachment.audioUrl
+        // adding a slashcommand button with required fields
+        extend.addAttachment({
+            color: "#2576F5",
+            actionButtonsAlignment: MessageActionButtonsAlignment.HORIZONTAL,
+            title: { value: "SpeechToText" },
+            text: `Queue audio file for transcription...`,
+            actions: [
+                {
+                    text: 'Transcribe',
+                    type: MessageActionType.BUTTON,
+                    msg_in_chat_window: true,
+                    msg: `/stt-queue ${rid} ${fileId} ${messageId} ${audioUrl}`,
+                },
+            ],
+        })
 
         return message
+
     }
 }
